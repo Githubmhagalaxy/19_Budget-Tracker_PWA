587cc75c07b11c5ec67feedf02b4c84a2e4f04e3
Michael <mhagalaxy@gmail.com>
2020-09-27 23:41:02 -0700

    final commit

---
+++ Develop/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// routes
+app.use(require("./routes/api.js"));
+
+
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`App running on port ${PORT}!`);
+        });
+    })
+    .catch(e => {
+        console.log(e.message);
+    })
+
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# 19_Budget-Tracker_PWA
+
+# Purpose:
+Build an application for tracking budget
+
+## screen shots
+![Budget Tracker](./screenshots/Annotation%202020-09-26%20204646.jpg)
+
+# Description:
+This challenge is to update an existing budget tracker application to allow for offline access and functionality. The user will be able to add expenses and deposits to their budget with or without a connection. If the user enters transactions offline, the total should be updated when they're brought back online. Once we’ve made these changes, we’ll deploy the application to Heroku.
+
+# Built With:
+* Express.js
+* MongoDB database
+* Mongoose ODM
+* Heroku
+
+## Deployed application
+Click [here](https://quiet-brook-15641.herokuapp.com/) to see deployed application
+
+# Contribution:
+Made by Mohammad H. Abbaspour
